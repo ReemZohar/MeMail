c519cf08439900baf61eb8a675bc30e8e467286f
lili <lianyam22@gmail.com>
2025-04-17 12:41:37 +0300

    tests for isMenuChoiceValid

---
+++ tests_validateUserInput.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include "isMenuChoiceValid_GPAPP23.h"
+#include <string>
+#include <vector>
+
+//GPAPP-29: Tests for isMenuChoiceValid (for sprint 1 is 1/2) (GPAPP-23)
+//Sanity:
+TEST(choiceTest, validChoice){
+    std::vector<std::string> choices = {"1", "2"};
+    EXPECT_TRUE(isMenuChoiceValid("1", choices));
+    EXPECT_TRUE(isMenuChoiceValid("2", choices));
+}
+//Negative + Egde + Boundary cases:
+TEST(choiceTest, inValidChoice){
+    std::vector<std::string> choices = {"1", "2"};
+    EXPECT_FALSE(isMenuChoiceValid("", choices));
+    EXPECT_FALSE(isMenuChoiceValid(" ", choices));
+    EXPECT_FALSE(isMenuChoiceValid("12", choices));  //invalid num
+    EXPECT_FALSE(isMenuChoiceValid("11111111111", choices));  //invalid num
+    EXPECT_FALSE(isMenuChoiceValid("A", choices));  //invalid char
+    EXPECT_FALSE(isMenuChoiceValid("a", choices)); //invalid char
+    EXPECT_FALSE(isMenuChoiceValid("!", choices)); //invalid char
+    EXPECT_FALSE(isMenuChoiceValid("0.1", choices)); //invalid num
+    EXPECT_FALSE(isMenuChoiceValid("1.1", choices)); //invalid num
+    EXPECT_FALSE(isMenuChoiceValid("-1", choices));  //invalid num
+    EXPECT_FALSE(isMenuChoiceValid("0", choices));  //invalid num
+    EXPECT_FALSE(isMenuChoiceValid("01", choices));  //invalid num
+    EXPECT_FALSE(isMenuChoiceValid("-1111", choices));  //invalid num
+    EXPECT_FALSE(isMenuChoiceValid("1.0", choices)); //float num
+}
