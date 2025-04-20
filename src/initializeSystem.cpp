968dce302f5262873552aea45b06e3542e2d3449
lili <lianyam22@gmail.com>
2025-04-20 23:22:23 +0300

    fixes - the parameters of the function till now

--- src/initializeSystem.cpp
+++ src/initializeSystem.cpp
@@ -7,24 +7,19 @@ using namespace std;
 #include <filesystem>
 
 // PGAPP-34
-// The function gets an existing file name and returns the BlackList from there as a vector
-vector<int> getBLFromBLFile(string fileName) {
-    fs::path currentPath = fs::current_path();
-    fs::path dataDir = currentPath.parent_path().parent_path() / "data"; 
+// The function gets an existing file path and returns the BlackList from there as a vector
+vector<int> getBLFromBLFile(const fs::path& filePath) {
+    ifstream file(filePath);
 
-    string path = (dataDir / fileName).string();
-    ifstream file(path);
-
-    // If the file couldn't open return an empty vector
     if (!file.is_open()) {
-        cerr << "Error: Could not open file " << path << endl;
+        cerr << "Error: Could not open file " << filePath << endl;
         return {};
     }
 
     vector<int> blacklist;
     string line1, line2;
     getline(file, line1); //Skip the 1st line
-    getline(file, line2); //Get the BlackList
+    getline(file, line2); //Blacklist values
 
     stringstream secLine(line2);
     int value;
@@ -32,17 +27,15 @@ vector<int> getBLFromBLFile(string fileName) {
         blacklist.push_back(value);
     }
 
-    file.close();
     return blacklist;
 }
 
 
 //PGAPP-35
-//The function gets a length and crear=te
-vector<int> createNewBLArr(string length) {
-    int len=stoi(length);
-    createNewBLFile(length);
-   return createZerosIntVec(len);
+//The function gets a length and a filepath, creates a new BL file and a new BL vector and returns it
+vector<int> createNewBLArr(const string& length, const fs::path& filePath) {
+    createNewBLFile(length, filePath);
+    return createZerosIntVec(stoi(length));
 }
 
 
@@ -54,37 +47,31 @@ vector<int> createZerosIntVec(int length) {
 
 
 //PGAPP-55:
-//The function gets a length of a new BL and creates a new file initialized with this length and a new zeros BL
-void createNewBLFile(string length) {
-    fs::path currentPath = fs::current_path();
-    fs::path projectRoot = currentPath.parent_path().parent_path();
-    fs::path dataDir = projectRoot / "data";
-    
+//The function gets a length of a new BL and a file path and creates a new file initialized with this length and a new zeros BL
+void createNewBLFile(const string& length, const fs::path& filePath) {
+    fs::path dataDir = filePath.parent_path();
+
     if (!fs::exists(dataDir)) {
         if (!fs::create_directory(dataDir)) {
-            cerr << "Can't create data directory!" << endl;
+            cerr << "Can't create data directory: " << dataDir << endl;
             return;
         }
     }
 
-    string path = dataDir / "BLFile.txt";
-    ofstream file(path);
+    ofstream file(filePath);
     int len = stoi(length);
+
     if (!file) {
-        cerr << "Can't create a new BlackList data File in:" << path << endl;
+        cerr << "Can't create file at: " << filePath << endl;
         return;
     }
 
     file << length << endl;
-
     for (int i = 0; i < len; i++) {
         file << "0";
-        if (i != len - 1) {
-            file << " ";
-        }
+        if (i != len - 1) file << " ";
     }
     file << endl;
-    file.close();
 }
 
 
--- src/initializeSystem.h
+++ src/initializeSystem.h
@@ -1,22 +1,23 @@
 #ifndef INITIALIZE_SYSTEM
 #define INITIALIZE_SYSTEM
-using namespace std;
+
 #include <vector>
 #include <string>
+#include <filesystem>
 
-//PGAPP-34
-vector<int> getBLFromBLFile(string fileName);
-    
-//PGAPP-35
-vector<int> createNewBLArr(string length);
+// PGAPP-34
+std::vector<int> getBLFromBLFile(const fs::path& filePath);
 
-//PGAPP-53
-vector<int> createZerosIntVec(int length);
+// PGAPP-35
+std::vector<int> createNewBLArr(const std::string& length, const fs::path& filePath);
 
-//PGAPP-55
-void createNewBLFile(string length);
+// PGAPP-53
+std::vector<int> createZerosIntVec(int length);
 
-//PGAPP-57
+// PGAPP-55
+void createNewBLFile(const std::string& length, const fs::path& filePath);
+
+// PGAPP-57
 std::vector<int> LoadBLFromFile(const std::string& length, const fs::path& filePath);
 
 #endif
