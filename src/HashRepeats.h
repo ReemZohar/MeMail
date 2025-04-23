46f6e9ea98cbe76d81b23147f8484db2189e17c6
ReemZohar <thefirstreem@gmail.com>
2025-04-23 23:27:24 +0300

    Implemented HashRepeats class to pass all tests

---
+++ src/HashRepeats.h
@@ -0,0 +1,23 @@
+#ifndef HASH_REPEATS
+#define HASH_REPEATS
+
+#include <string>
+#include <functional>
+
+class HashRepeats {
+    public:
+    //constructor
+    HashRepeats(std::function<std::size_t(std::size_t)> hashFunc, int count);
+
+    //getter    
+    int getRepeatCount() const;
+
+    //activates the hash function on the given integer and returns the result
+    std::size_t hash(std::size_t target) const;
+
+    private:
+    //class fields    
+    std::function<std::size_t(std::size_t)> hashFunc; //the hash function we'll use
+    int repeatCount; //how many times the hash function is repeated
+};
+#endif
