a471b233ccac90891c7f8780b6fdb21cc897d20f
ReemZohar <thefirstreem@gmail.com>
2025-04-23 19:23:38 +0300

    Implemented firstUserSelection function

--- src/userAction.cpp
+++ src/userAction.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+string firstUserSelection() {
+    string userSelection;
+
+    /*loop take a line of input representing the black list size and hash functions selected by the user,
+     and iterates until the choice is valid.*/
+    do {
+        getline(cin, userSelection);
+    } while(VALIDATE_USER_INPUT::isBLSizeSpaceHashsInputValid(userSelection) == false);
+
+    return userSelection;
+}
+
 vector<int> convStringToArr(const string& userInput) {
     vector<int> userChoices;
     string n = "", init = "";
--- src/userAction.h
+++ src/userAction.h
@@ -1,10 +1,17 @@
 #ifndef USER_ACTION
 #define USER_ACTION
 
+#include "validateUserInput.h"
 #include <string>
 #include <vector>
+#include <iostream>
 
+//PGAPP-65
 //takes the user line of input and converts it to an integer vector
 std::vector<int> convStringToArr(const std::string& userInput);
+
+//PGAPP-70
+//returns the line of input given by the user in the format: [blacklistSize] [hash1 repetitions] ... [hashN repetitions]
+std::string firstUserSelection();
 
 #endif
