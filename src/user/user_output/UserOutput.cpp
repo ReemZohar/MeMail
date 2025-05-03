f6b2dc27520d4484f51c6de267ad68c58bc3b35e
Noa <noabiber44@gmail.com>
2025-05-03 14:28:17 +0300

    PGAPP-97-Implement-UserOutput-class-to-pass-all-tests

---
+++ src/user/user_output/UserOutput.cpp
@@ -0,0 +1,18 @@
+#include "UserOutput.h"
+#include <iostream>
+#include <fstream>
+
+//PGAPP-97-UserOutput-class
+void UserOutput::printToConsole(const std::string& message) {
+    std::cout << message << std::endl;
+}
+
+void UserOutput::writeToFile(const std::string& filepath, const std::string& content) {
+    std::ofstream outFile(filepath, std::ios::out | std::ios::app);
+    if (outFile.is_open()) {
+        outFile << content;
+        outFile.close();
+    } else {
+        printToConsole("Unable to open file for writing: " + filepath);
+    }
+}
