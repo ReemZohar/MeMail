bc2880d5beda489449e8831d0df1ce01e67001a7
liany2 <lianyam22@gmail.com>
2025-05-04 12:43:32 +0300

    commit 4

--- src/main.cpp
+++ src/main.cpp
@@ -10,33 +10,34 @@
 #include "IHasher.h"
 #include "HashRepeats.h"
 using namespace std;
+namespace fs = std::filesystem; 
 
 int main() {
     //FirstUser input for initialization
     FirstUserInput userInput; 
     userInput.takeInput();
-    std::string inputLine = userInput.getInput(); 
+    string inputLine = userInput.getInput(); 
+
+    vector <int> firstInputVec= USER_ACTION::convStringToArr(inputLine);
 
     //initialize system
-    fs::path testDir = fs::current_path() / "data";  // Use project directory + data
-    fs::create_directories(testDir);                 // Create 'data' if it doesn't exist
-    fs::path filePath = testDir / "BLFile.txt";      // Path to final file
-      
-    int sizeBl = USER_ACTION::convStringToArr(inputLine).at(0);
-    std::vector<bool> blList = INITIALIZE_SYSTEM::loadBLFromFile(std::to_string(sizeBl),filePath);
-    
-    vector<function<size_t(size_t)>> funcs = createHashVec(sizeBl);
+    fs::path BlFilePath = fs::current_path() / "data"/ "BLFile.txt";  //The path to the file we want to handle
+    int sizeBl = firstInputVec.at(0);
+
+    vector<bool> blackList = INITIALIZE_SYSTEM::loadBLFromFile(to_string(sizeBl),BlFilePath);
+
+    vector<function<size_t(size_t)>> funcs = createHashVec(firstInputVec.size()-1);
     vector<shared_ptr<IHasher>> hasher = convInputToHashRepeatsVec(inputLine, funcs);
  
     //Create bloomFilter object
-    BloomFilter bl(blList, filePath,hasher);
+    BloomFilter bl(blackList, BlFilePath,hasher);
     //take input from user
     MenuChoiceInput mc;
 
     while (true)
     {
         mc.takeInput();
-        std::string inputLine = mc.getInput();
+        string inputLine = mc.getInput();
         shared_ptr<IUserInput> mci1 = make_shared<MenuChoiceInput>(inputLine);
 
         shared_ptr<IAction> actionObj = ActionFactory::create(bl, mci1);
