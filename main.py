674a393c7d4bcee09bc7f6af772f58a2851539f8
liany2 <lianyam22@gmail.com>
2025-05-07 00:21:36 +0300

    create the main.py function of the Client

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from Client import Client
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <server_ip> <port>")
+        sys.exit(1)
+
+    server_ip = sys.argv[1]
+    server_port = int(sys.argv[2])
+
+    client = Client(server_ip, server_port)
+    client.start()
+
+    while True:
+        msg = input()
+        client.sendMessage(msg)
+        data = client.receiveMessage()
+        print(data)
