4f7e53564e02b3c6215265740e21868343d07692
liany2 <lianyam22@gmail.com>
2025-05-07 00:03:03 +0300

    Implement sendMessage function

---
+++ Client.py
@@ -0,0 +1,16 @@
+import socket
+from IClient import Iclient
+
+class Client(Iclient):
+    def __init__(self, dest_IP: str, dest_port: int):
+        self.dest_IP = dest_IP
+        self.dest_port = dest_port
+        self.s = None
+
+    def start(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((self.dest_IP, self.dest_port))
+        print("Connected to server.")  #todo delete
+
+    def sendMessage(self, msg: str):
+        self.s.send(bytes(msg, 'utf-8'))
