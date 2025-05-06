eaaa31879d12d370dd7ee410015d65e7b5374b3a
ReemZohar <thefirstreem@gmail.com>
2025-05-06 11:17:41 +0300

    Implemented BadAction class

--- src/user/user_output/OutputToClient.cpp
+++ src/user/user_output/OutputToClient.cpp
@@ -1,5 +1,9 @@
 #include "OutputToClient.h"
 
+using namespace std;
+
+OutputToClient::OutputToClient(string output) : output(output) {}
+
 //TODO - add implementation
 bool OutputToClient::shareOutput() {
     return true;
