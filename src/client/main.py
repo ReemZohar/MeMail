a44ee06130b1d22c8b29ac9d84d9b54bf8c48027
liany2 <lianyam22@gmail.com>
2025-05-12 19:13:30 +0300

    addings a check for a port validation in python

--- src/client/main.py
+++ src/client/main.py
@@ -2,12 +2,18 @@ import sys
 from Client import Client
 
 if __name__ == "__main__":
-
     if len(sys.argv) != 3:
         sys.exit(1)
 
     server_ip = sys.argv[1]
-    server_port = int(sys.argv[2])
+
+    try:
+        server_port = int(sys.argv[2])
+    except ValueError:
+        sys.exit(1)
+
+    if not (0 <= server_port <= 65535):
+        sys.exit(1)
 
     client = Client(server_ip, server_port)  #Create a new Client object
     client.start()  #Upload the client
