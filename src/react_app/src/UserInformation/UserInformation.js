4b6ca456709f917dc6e25cd79bfb0d1c63b6959b
ReemZohar <thefirstreem@gmail.com>
2025-06-11 15:08:00 +0300

    Finished front implementation of UserInformation component

--- src/react_app/src/UserInformation/UserInformation.js
+++ src/react_app/src/UserInformation/UserInformation.js
@@ -1,23 +1,63 @@
 import "./UserInformation.css"
 
-function UserInformation({ requiredInfo, theme, className="form-control", type="text" }) {
-    return (
+function UserInformation({ requiredInfo, theme, type="text", isValid="", feedback="", min="", max="" }) {
+    //valid user information scenario
+    if(isValid === true) {
+        return (
         <div class="form-floating" data-bs-theme={theme}>
-            <input type={type} class={className} id="floatingInput" placeholder=""></input>
+            <input type={type} class="form-control is-valid" id="floatingInput" placeholder="" min={min} max={max}></input>
             <label data-bs-theme={theme} for="floatingInput">{requiredInfo}</label>
         </div>
-    )
+        )
+    }
+    //invalid user information scenario
+    else if(isValid === false) {
+        return (
+        <div class="form-floating" data-bs-theme={theme}>
+            <input type={type} class="form-control is-invalid" id = "floatingInput" placeholder="" min={min} max={max}></input>
+            <label data-bs-theme={theme} for="floatingInput">{requiredInfo}</label>
+            <div class="invalid-feedback">
+                {feedback}
+            </div>
+        </div>
+        )
+    }
+    //no feedback user information form scenario
+    else {
+        return (
+        <div class="form-floating" data-bs-theme={theme}>
+            <input type={type} class="form-control" id = "floatingInput" placeholder="" min={min} max={max}></input>
+            <label data-bs-theme={theme} for="floatingInput">{requiredInfo}</label>
+        </div>
+        )
+    }
 }
 
 //password form component
-UserInformation.Password = props => (
-    <UserInformation {...props} type="password" />
-)
+UserInformation.Password = props => {
+    //TODO: password validation function
+    //props.isValid =
+    //props.feedback =
+    return (
+        <UserInformation {...props} type="password" />
+    )
+}
 
+//number form component
+UserInformation.Num = props => {
+    //TODO: password validation function
+    //props.isValid =
+    //props.feedback =
+    return (
+        <UserInformation {...props} type="number"/>
+    )
+}
+
+//month form component
 UserInformation.Month = ({ theme }) => {
     return (
         <div class="form-floating" data-bs-theme={theme}>
-            <select class="form-select date" id="floatingSelect">
+            <select class="form-select" id="floatingSelect">
                 <option value="1">January</option>
                 <option value="2">February</option>
                 <option value="3">March</option>
@@ -36,9 +76,10 @@ UserInformation.Month = ({ theme }) => {
     )
 }
 
+//gender form component
 UserInformation.Gender = ({ theme }) => {
     return (
-        <div class="form-floating" data-bs-theme={theme}>
+        <div class="form-floating col-4-5" data-bs-theme={theme}>
             <select class="form-select" id="floatingSelect">
                 <option value="1">Female</option>
                 <option value="2">Male</option>
@@ -49,9 +90,27 @@ UserInformation.Gender = ({ theme }) => {
     )
 }
 
-UserInformation.Date = props => (
-    <UserInformation {...props} className="form-control date" />
-)
+//day month and year form component
+UserInformation.Date = ({ theme }) => {
+    //TODO: date validation function
+    //const isValid =
+    //const feedback =
+    const date = new Date();
+    const currYear = date.getFullYear();
 
+    return (
+        <div class="row g-2">
+            <div class="col-1-5">
+                <UserInformation.Num requiredInfo={"Day"} theme={theme} min={1} max={31}></UserInformation.Num> 
+            </div>
+            <div class="col-1-5">
+                <UserInformation.Month theme={theme}></UserInformation.Month>
+            </div>
+            <div class="col-1-5">
+                <UserInformation.Num requiredInfo={"Year"} theme={theme} min={1} max={currYear}></UserInformation.Num>
+            </div>
+        </div>
+    )
+}
 
 export default UserInformation
--- src/react_app/src/index.js
+++ src/react_app/src/index.js
@@ -5,11 +5,6 @@ import UserInformation from './UserInformation/UserInformation'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserInformation requiredInfo={"Lian"} theme={"dark"}/>
-    <UserInformation.Password requiredInfo={"Lian"} theme={"light"}/>
-    <UserInformation.Month theme={"dark"}/>
-    <UserInformation.Gender theme={"dark"}/>
-    <UserInformation.Date requiredInfo={"Year"} theme={"dark"}/>
-    <UserInformation.Date requiredInfo={"Day"} theme={"dark"}/>
+    <div>nothing here yet</div>
   </React.StrictMode>
 );
