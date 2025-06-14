c3e9bb7447d7ef81075d7aeb9147300efdc52c64
liany2 <lianyam22@example.com>
2025-06-14 16:14:36 +0300

    handling communication with Node.js server

--- src/react_app/src/index.js
+++ src/react_app/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MdInbox } from 'react-icons/md';
 import UserInformation from './UserInformation/UserInformation'
+import LeftMenu from './LeftMenu/LeftMenu'
+import CustomLabelMenu from './CustomLabelMenu/CustomLabelMenu'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>nothing here yet</div>
+    <BrowserRouter>
+      <Routes>
+        {/*All pages*/}
+      </Routes>
+      {/* Static - in all pages*/}
+         <LeftMenu
+      theme="light"
+      activeLabelId={null}
+      clickOnLabel={(id) => console.log('Label clicked:', id)}
+      onComposeClick={() => console.log('Compose clicked')}
+    />
+    </BrowserRouter>
   </React.StrictMode>
 );
