efe09c403b39d855f42ba49afbbea991b0f149b7
liany2 <lianyam22@example.com>
2025-06-14 16:58:33 +0300

    relevant index.js for checking

--- src/react_app/src/index.js
+++ src/react_app/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import UserInformation from './UserInformation/UserInformation'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MdInbox } from 'react-icons/md';
+import UserInformation from './UserInformation/UserInformation';
+import LeftMenu from './LeftMenu/LeftMenu';
+import CustomLabelMenu from './CustomLabelMenu/CustomLabelMenu';
+import DeleteMailButton from './DeleteMailButton/DeleteMailButton'; 
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>nothing here yet</div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          {/* All pages */}
+        </Routes>
+        {/* Static - in all pages */}
+        <DeleteMailButton onClick={() => console.log('Delete clicked!')} />
+
+      </div>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
