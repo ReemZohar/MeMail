81c8d185d61aa417a0207c6bc6ca4284971255be
liany2 <lianyam22@gmail.com>
2025-06-16 19:33:02 +0300

    Update NewMailButton.js

--- src/react_app/src/NewMailButton/NewMailButton.js
+++ src/react_app/src/NewMailButton/NewMailButton.js
@@ -1,17 +1,17 @@
+import React from "react";
+import { MdCreate } from "react-icons/md";
 import "./NewMailButton.css";
-import { MdEdit } from "react-icons/md";
 
-function NewMailButton({ theme, onClick }) {
-    return (
-        <button
-            className="compose-button"
-            data-bs-theme={theme}
-            onClick={onClick}
-        >
-            <MdEdit className="compose-icon" />
-            Compose
-        </button>
-    );
+export default function NewMailButton({ theme, isCollapsed, onClick }) {
+  return (
+    <button
+      className={`newMail-button ${theme === "dark" ? "dark" : "light"} ${
+        isCollapsed ? "collapsed" : ""
+      }`}
+      onClick={onClick}
+    >
+      <MdCreate size={20} className="newMail-icon" />
+      {!isCollapsed && <span className="text">Compose</span>}
+    </button>
+  );
 }
-
-export default NewMailButton;
