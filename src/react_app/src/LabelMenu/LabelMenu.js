764fd30e003027ca2c7b3d2c396a79caf9e26109
liany2 <lianyam22@example.com>
2025-06-16 17:55:03 +0300

    drafts

--- src/react_app/src/LabelMenu/LabelMenu.js
+++ src/react_app/src/LabelMenu/LabelMenu.js
@@ -1,11 +1,11 @@
 import './LabelMenu.css';
-import { MdInbox, MdSend, MdDrafts, MdMail, MdReport, MdStar } from 'react-icons/md';
+import { MdInbox, MdSend, MdOutlineDrafts, MdMail, MdReport, MdStar } from 'react-icons/md';
 
 function LabelMenu({ theme, onLabelClick, activeFolder, isCollapsed }) {
   const labels = [
     { id: 'inbox', name: 'Inbox', icon: <MdInbox size={18} />, isFavorite: false },
     { id: 'sent', name: 'Sent', icon: <MdSend size={18} />, isFavorite: false },
-    { id: 'drafts', name: 'Drafts', icon: <MdDrafts size={18} />, isFavorite: false },
+    { id: 'drafts', name: 'Drafts', icon: <MdOutlineDrafts size={18} />, isFavorite: false },
     { id: 'allmail', name: 'All Mail', icon: <MdMail size={18} />, isFavorite: false },
     { id: 'favorite', name: 'Favorite', icon: <MdStar size={18} />, isFavorite: true },
     { id: 'spam', name: 'Spam', icon: <MdReport size={18} />, isFavorite: false },
