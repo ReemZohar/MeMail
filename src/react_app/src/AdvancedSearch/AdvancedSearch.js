463a60588d4fb843d9a1986cbd821854029d347f
ReemZohar <thefirstreem@gmail.com>
2025-06-18 18:59:21 +0300

    tied between the SearchBar component and the AdvancedSearch component

--- src/react_app/src/AdvancedSearch/AdvancedSearch.js
+++ src/react_app/src/AdvancedSearch/AdvancedSearch.js
@@ -2,22 +2,7 @@ import './AdvancedSearch.css';
 import AdvSearchForm from '../AdvSearchForm/AdvSearchForm';
 import ThemeButton from '../ThemeButton/ThemeButton';
 
-function AdvancedSearch({
-    theme,
-    fromVal,
-    onChgFrom,
-    toVal,
-    onChgTo,
-    subVal,
-    onChgSub,
-    incVal,
-    onChgInc,
-    notIncVal,
-    onChgNotInc,
-}) {
-    const handleNext = () => {
-
-    }
+function AdvancedSearch({ theme }) {
 
     return (
         <div data-bs-theme={theme} class="card search-card">
@@ -29,7 +14,7 @@ function AdvancedSearch({
                 <AdvSearchForm filter={"Doesn't have"} />
             </div>
             <div className="d-flex justify-content-end mb-1 me-1">
-                <ThemeButton theme={theme} btnText={"Search"} handleNext={handleNext} />
+                <ThemeButton theme={theme} btnText={"Search"} />
             </div>
         </div>
     )
--- src/react_app/src/SearchBar/SearchBar.js
+++ src/react_app/src/SearchBar/SearchBar.js
@@ -1,19 +1,16 @@
 import React, { useState, useRef } from 'react';
 import { MdSearch, MdClose, MdTune } from 'react-icons/md';
+import AdvancedSearch from '../AdvancedSearch/AdvancedSearch';
 import './SearchBar.css';
 
 function SearchBar() {
   const [query, setQuery] = useState('');
+  const [showAdvanced, setShowAdvanced] = useState(false);
   const inputRef = useRef(null);
 
   const clearInput = () => {
     setQuery('');
     inputRef.current.focus();
-  };
-
-  const openAdvancedSearch = () => {
-    //Advanced search logic
-    alert('Open advanced search modal'); //todo delete
   };
 
   const performSearch = () => {
@@ -27,32 +24,41 @@ function SearchBar() {
   };
 
   return (
-    <div className="search-bar">
-      <button
-        type="button"
-        className="search-icon-button"
-        onClick={performSearch}
-        aria-label="Search"
-      >
-        <MdSearch className="search-icon" />
-      </button>
-      <input
-        ref={inputRef}
-        type="text"
-        placeholder="Search mail"
-        aria-label="Search mail"
-        value={query}
-        onChange={e => setQuery(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
-      {query && (
-        <button className="clear-button" onClick={clearInput} aria-label="Clear search">
-          <MdClose size={20} />
+    <div>
+      <div className="search-bar">
+        <button
+          type="button"
+          className="search-icon-button"
+          onClick={performSearch}
+          aria-label="Search"
+        >
+          <MdSearch className="search-icon" />
         </button>
-      )}
-      <button className="advanced-button" onClick={openAdvancedSearch} aria-label="Advanced search">
-        <MdTune size={20} />
-      </button>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Search mail"
+          aria-label="Search mail"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        {query && (
+          <button className="clear-button" onClick={clearInput} aria-label="Clear search">
+            <MdClose size={20} />
+          </button>
+        )}
+        <button className="advanced-button" onClick={() => setShowAdvanced(!showAdvanced)} aria-label="Advanced search">
+          <MdTune size={20} />
+        </button>
+      </div>
+      <div>
+        {showAdvanced && (
+          <AdvancedSearch
+            theme={"light"}
+          />
+        )}
+      </div>
     </div>
   );
 }
